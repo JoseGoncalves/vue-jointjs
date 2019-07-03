ccef7ad4dd9487c8cb8dde68ccd69b8e4d233434
José Miguel Gonçalves <jose.goncalves@inov.pt>
2019-07-03 13:39:04 +0100

    Slightly more optimized JointJS import.

--- src/plugins/rappid.js
+++ src/plugins/rappid.js
@@ -1,9 +1,12 @@
 import 'jointjs/dist/joint.core.css';
 
-import * as joint from 'jointjs/dist/joint.core';
+import { dia } from 'jointjs/src/core.mjs';
+import * as standard from 'jointjs/src/shapes/standard.mjs';
 
 export default {
 	install: function (Vue) {
+		let joint = { dia };
+		joint.shapes = { standard };
 		Object.defineProperty(Vue.prototype, '$joint', { value: joint });
 	}
 };
