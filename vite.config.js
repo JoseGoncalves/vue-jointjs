c35545686eeacf2531e8190bd9b9487b47a9bd97
José Miguel Gonçalves <jose.goncalves@inov.pt>
2023-07-24 15:15:46 +0100

    Disable Options API support. Increase limit of chunk size warning.

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,17 @@ export default defineConfig({
 		port: 8080
 	},
 	plugins: [vue()],
+	define: {
+		// Disable Options API support
+		// https://github.com/vuejs/core/tree/main/packages/vue#bundler-build-feature-flags
+		__VUE_OPTIONS_API__: false
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
+	},
+	build: {
+		chunkSizeWarningLimit: 750
 	}
 });
